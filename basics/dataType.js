959c9e2c5516d0efe243576c1db7d18f7db765d3
Gyanendra Behera <gyanb230@gmail.com>
2024-06-17 09:39:34 +0530

    Basic

---
+++ basics/dataType.js
@@ -0,0 +1,20 @@
+"useStrict"; //treated all js code as new version
+
+//data type
+/*
+primitive dataType
+1. number=>range 0->2**53
+2. bigint
+3. string=>""
+4. boolean=> true/false
+5. null=>standalone value
+6. undefined=>
+7. symbol=> unique
+
+user defined 
+1. object
+
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ basics/variable.js
@@ -0,0 +1,5 @@
+const id = 123;
+let salary = 10000;
+var name = "Rohan";
+//prefer not to use var due to scope and functional scope issue.
+console.table([id, salary, name]);
