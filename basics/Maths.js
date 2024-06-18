274a2e3020b968436e238504dd65d91e64d4ffc9
Gyanendra Behera <gyanb230@gmail.com>
2024-06-18 13:14:19 +0530

    maths

---
+++ basics/Maths.js
@@ -0,0 +1,10 @@
+let value = Math.floor(Math.random() * 6 + 1);
+
+console.log(value);
+
+//lets bind it in a range
+let min = 10;
+let max = 30;
+
+let num = Math.floor(Math.random() * (max - min) + 1) + min;
+console.log(num);
